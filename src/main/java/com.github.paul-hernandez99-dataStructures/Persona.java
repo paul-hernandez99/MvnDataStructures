4e36aba5776df6c5f8521e080b0a497f14ac40f3
Paul Hernandez <paul.hernandez@opendeusto.es>
2021-05-18 21:17:08 +0200

    Update Persona.java

---
+++ src/main/java/com.github.paul-hernandez99-dataStructures/Persona.java
@@ -0,0 +1,31 @@
+package main.java.com.github.paul-hernandez99;
+
+public class Persona {
+  private String nombre;
+  private int edad;
+  private int dni;
+
+  public Persona (String nombre, int edad, int dni){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  public String getNombre() {
+    return this.nombre;
+  }
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+  public int getEdad() {
+    return this.edad
+  }
+  public void setDni(int dni) {
+    this.dni = dni;
+  }
+  public int getDni() {
+    return this.dni;
+  }
+}
